547cc6361e72c3917fa86be367b4634abdbed5c1
zanthrash <Zan.Thrash@target.com>
2020-11-25 14:19:14 -0600

    created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,4 +1,10 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
