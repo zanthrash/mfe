407caae0b78a4e9dbdb8c34ae391a46984a2209a
zanthrash <zanthrash@gmail.com>
2020-12-13 15:08:56 -0600

    namespace classnames for css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { MarketingApp } from "./component/MarketingApp";
 import Header from "./component/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
